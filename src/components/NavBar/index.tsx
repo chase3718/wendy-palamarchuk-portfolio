a58a9a1f5cf65deab226f7dbf30bb3803429f01b
Chase Williams <chasewilliams98@gmail.com>
2022-06-22 16:52:04 -0400

    Added routing

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavButton from './components/NavButton'
 import Title from './components/Title'
 import './styles.css'
+import $ from 'jquery';
 
 export default function NavBar() {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
   return (
     <div className="navbar">
-        <Title />
-        <NavButton title="about me" />
-        <NavButton title="projects"/>
+      <NavButton className="title" title="Wendy Palamarchuk" pathTo='/aboutMe' />
+      <NavButton className="navigation" title="about me" pathTo="/aboutMe" />
+      <NavButton className="navigation" title="projects" pathTo="/projects" />
     </div>
   )
 }
